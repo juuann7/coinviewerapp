c130badf829be2357f87fbc7c261164aa8c12719
juuann7 <muhamadnajwan20@gmail.com>
2024-12-24 23:23:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'package:coinviewer/models/app_config.dart';
+import 'package:coinviewer/pages/HomePage.dart';
+import 'package:coinviewer/services/http_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
+  registerHTTPService();
+  await GetIt.instance.get<HTTPService>().get("/coins/bitcoin");
+  runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String configContent = await rootBundle.loadString('assets/config/main.json');
+  Map<String, dynamic> configData = jsonDecode(configContent);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(
+      COIN_API_BASE_URL: configData["COIN_API_BASE_URL"],
+    ),
+  );
+}
+
+void registerHTTPService() {
+  GetIt.instance.registerSingleton<HTTPService>(
+    HTTPService(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coin Viewer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 229, 145, 212),
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/services/http_service.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import '../models/app_config.dart';
+import 'package:get_it/get_it.dart';
+
+class HTTPService {
+  final Dio dio = Dio();
+
+  AppConfig? _appConfig;
+  String? _base_url;
+
+  HTTPService() {
+    _appConfig = GetIt.instance.get<AppConfig>();
+    _base_url = _appConfig!.COIN_API_BASE_URL;
+    print(_base_url);
+  }
+
+  Future<Response?> get(String path) async {
+    try {
+      String _url = "$_base_url$path";
+      print(_url);
+      Response _response = await dio.get(_url);
+      print(_response);
+      return _response;
+    } catch (e) {
+      print('HTTPService: Tidak bisa mendapatkan request.');
+      print(e);
+      return null;
+    }
+  }
+}
